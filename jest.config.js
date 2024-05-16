48064b4f6071e7eaa7820eba278eb166c98ed9ed
Santosh Shinde <santoshshinde2012@gmail.com>
2024-05-16 23:08:01 +0530

    update report

--- jest.config.js
+++ jest.config.js
@@ -4,8 +4,8 @@ module.exports = {
   roots: ['./tests'],
   coveragePathIgnorePatterns: ['/node_modules/', '/tests/'],
   collectCoverage: true,
-  reporters: ['default', ['./dist/index.js', {
-    outputFile: './coverage/sonar-report.xml',
-  }]],
+  // reporters: ['default', ['./dist/index.js', {
+  //   outputFile: './coverage/sonar-report.xml',
+  // }]],
   testEnvironment: 'node',
 };
